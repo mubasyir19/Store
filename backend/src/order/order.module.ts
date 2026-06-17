715f1a9f17bb37d5cf3100849fddfb1fc2ab10b2
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2026-06-17 08:46:26 +0700

    fix(backend): add import config service in order module

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -4,9 +4,10 @@ import { OrderController } from './order.controller';
 import { PrismaService } from 'src/prisma.service';
 import { MidtransService } from './midtrans.service';
 import { WebhookController } from './webhook.controller';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [OrderController, WebhookController],
-  providers: [OrderService, PrismaService, MidtransService],
+  providers: [OrderService, PrismaService, MidtransService, ConfigService],
 })
 export class OrderModule {}
